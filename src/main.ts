af50a477b27cbbc85779b3f1bf3df12e92a4b773
QuangTruong <nqtruong2210@gmail.com>
2024-03-03 18:00:49 +0700

    add: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('FIVERR API')
+    .addBearerAuth()
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+
+  const swagger = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, swagger);
   await app.listen(3000);
 }
 bootstrap();
