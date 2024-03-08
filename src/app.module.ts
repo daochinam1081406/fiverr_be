fd6181725c744b52329713b8c7ae2dbc1e524b50
QuangTruong <nqtruong2210@gmail.com>
2024-03-08 21:39:12 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { JobModule } from './job/job.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { UserModule } from './user/user.module';
     ConfigModule.forRoot({ isGlobal: true }),
     ServeStaticModule.forRoot({ rootPath: '.' }),
     UserModule,
+    CommentModule,
+    JobModule,
   ],
   controllers: [AppController],
   providers: [AppService],
