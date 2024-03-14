c0fd64613d2a563956bc82536333207b0067f05e
QuangTruong <nqtruong2210@gmail.com>
2024-03-14 22:26:46 +0700

    fix

--- src/job-detail-type/job-type-detail.service.ts
+++ src/job-detail-type/job-type-detail.service.ts
@@ -1,48 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateJobTypeDetailDto } from './dto/create-job-type-detail.dto';
 import { UpdateJobTypeDetailDto } from './dto/update-job-type-detail.dto';
 import { PrismaClient } from '@prisma/client';
-import * as moment from 'moment';
 
 @Injectable()
 export class JobTypeDetailService {
   prisma = new PrismaClient();
 
-  async findAll() {
-    const data = {
-      content: {},
-      dateTime: moment().format('YYYY-MM-DD HH:mm:ss'),
-    };
-    return data;
+  async findAll(): Promise<any> {
+    try {
+      let data = await this.prisma.jobDetailType.findMany({});
+      return data;
+    } catch (error) {
+      throw new NotFoundException('Failed to retrieve job detail types.');
+    }
   }
 
   async create(
     createJobTypeDetailDto: CreateJobTypeDetailDto,
   ): Promise<string> {
-    let newTypeDetail = { ...createJobTypeDetailDto };
-    await this.prisma.jobDetailType.create({
-      data: newTypeDetail,
-    });
-    return `Created Success`;
+    try {
+      let newTypeDetail = { ...createJobTypeDetailDto };
+      await this.prisma.jobDetailType.create({
+        data: newTypeDetail,
+      });
+      return ' Created Success';
+    } catch (error) {
+      throw new NotFoundException('Failed to create job detail type.');
+    }
   }
+
   async findWidthPage(
     skip: number,
     numSize: number,
     keyword: string,
   ): Promise<any> {
-    const data = {
-      content: {},
-      dateTime: moment().format('YYYY-MM-DD HH:mm:ss'),
-    };
-    return data;
+    try {
+      if (isNaN(skip) || isNaN(numSize) || !keyword) {
+        return { error: 'Please input param' };
+      }
+      let data = await this.prisma.jobDetailType.findMany({
+        where: {
+          detail_name: {
+            contains: keyword,
+          },
+        },
+        skip: skip,
+        take: numSize,
+      });
+      return data;
+    } catch (error) {
+      throw new NotFoundException(
+        'Failed to find job detail types with pagination.',
+      );
+    }
   }
 
   async findOne(job_detail_type_id: number): Promise<any> {
-    const data = {
-      content: {},
-      dateTime: moment().format('YYYY-MM-DD HH:mm:ss'),
-    };
-    return data;
+    try {
+      let data = await this.prisma.jobDetailType.findMany({
+        where: {
+          job_detail_type_id: job_detail_type_id,
+        },
+      });
+      return data;
+    } catch (error) {
+      throw new NotFoundException('Failed to find job detail type.');
+    }
   }
 
   async upload(
@@ -50,37 +74,51 @@ export class JobTypeDetailService {
     job_detail_type_id: number,
     updateJobTypeDetailDto: UpdateJobTypeDetailDto,
   ): Promise<string> {
-    let upload = { ...updateJobTypeDetailDto };
-    upload.image = filename;
-    await this.prisma.jobDetailType.update({
-      where: {
-        job_detail_type_id: job_detail_type_id,
-      },
-      data: upload,
-    });
-    return `Upload Image success !`;
+    try {
+      let upload = { ...updateJobTypeDetailDto };
+      upload.image = filename;
+      await this.prisma.jobDetailType.update({
+        where: {
+          job_detail_type_id: job_detail_type_id,
+        },
+        data: upload,
+      });
+      return 'Upload Image success'!;
+    } catch (error) {
+      throw new NotFoundException(
+        'Failed to upload image for job detail type.',
+      );
+    }
   }
 
   async update(
     job_detail_type_id: number,
     updateJobTypeDetailDto: UpdateJobTypeDetailDto,
   ): Promise<string> {
-    let updateTypeDetail = { ...updateJobTypeDetailDto };
-    await this.prisma.jobDetailType.update({
-      where: {
-        job_detail_type_id: job_detail_type_id,
-      },
-      data: updateTypeDetail,
-    });
-    return `update success!`;
+    try {
+      let updateTypeDetail = { ...updateJobTypeDetailDto };
+      await this.prisma.jobDetailType.update({
+        where: {
+          job_detail_type_id: job_detail_type_id,
+        },
+        data: updateTypeDetail,
+      });
+      return 'update success!';
+    } catch (error) {
+      throw new NotFoundException('Failed to update job detail type.');
+    }
   }
 
   async remove(job_detail_type_id: number): Promise<string> {
-    await this.prisma.jobDetailType.delete({
-      where: {
-        job_detail_type_id: job_detail_type_id,
-      },
-    });
-    return `delete  suscessfull! `;
+    try {
+      await this.prisma.jobDetailType.delete({
+        where: {
+          job_detail_type_id: job_detail_type_id,
+        },
+      });
+      return 'delete  suscessfull!';
+    } catch (error) {
+      throw new NotFoundException('Failed to delete job detail type.');
+    }
   }
 }
